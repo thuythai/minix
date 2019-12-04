bd4b7d394e740ae1896c3d2fc4cc22f469a168e0
Thùy Thái <31557230+thuythai@users.noreply.github.com>
2019-12-04 17:30:42 -0500

    Update mempolicy.c

--- minix/servers/pm/mempolicy.c
+++ minix/servers/pm/mempolicy.c
@@ -7,7 +7,8 @@
 
 int do_cyclemempolicy(void){
     //CUSTOM_MEM_POLICY = (CUSTOM_MEM_POLICY + 1) % 5;
-    CUSTOM_MEM_POLICY = selection;
+    //CUSTOM_MEM_POLICY = selection;
+    CUSTOM_MEM_POLICY = 0;
 
     return(OK);
 }
