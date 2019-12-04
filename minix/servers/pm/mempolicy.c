af6f8c246a466172cb7e10bd5c560549abaa35e0
Thùy Thái <31557230+thuythai@users.noreply.github.com>
2019-12-04 16:12:17 -0500

    added parameter selection for the syscall

--- minix/servers/pm/mempolicy.c
+++ minix/servers/pm/mempolicy.c
@@ -5,8 +5,9 @@
 #include "mproc.h"
 #include "memheader.c"
 
-int do_cyclemempolicy(void){
-    CUSTOM_MEM_POLICY = (CUSTOM_MEM_POLICY + 1) % 5;
+int do_cyclemempolicy(int selection){
+    //CUSTOM_MEM_POLICY = (CUSTOM_MEM_POLICY + 1) % 5;
+    CUSTOM_MEM_POLICY = selection;
 
     return(OK);
 }
