1d96f8d0ca7dc2999f347899337a6d8a23d567c6
Thùy Thái <31557230+thuythai@users.noreply.github.com>
2019-12-09 18:11:57 -0500

    Update and rename mempolicy.c to memSelection.c

--- minix/servers/pm/memSelection.c
+++ minix/servers/pm/memSelection.c
@@ -3,11 +3,11 @@
 #include <minix/com.h>
 #include <signal.h>
 #include "mproc.h"
-#include "memheader.c"
+#include "memSelectionHeader.c"
 
-int do_cyclemempolicy(void){
+int do_selectmempolicy(void){
     //CUSTOM_MEM_POLICY = 0;
-    printf("value of m_in.m1_i1 is %d\n", m_in.m1_i1);
+    //printf("value of m_in.m1_i1 is %d\n", m_in.m1_i1);
     CUSTOM_MEM_POLICY = m_in.m1_i1;
     printf("CUSTOM_MEM_POLICY is set to: %d\n", CUSTOM_MEM_POLICY);
     return(OK);
