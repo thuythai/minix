9f636490038f533afc51b6ff311e0fe671fb09c8
Thùy Thái <31557230+thuythai@users.noreply.github.com>
2019-12-09 19:01:47 -0500

    Update memSelection.c

--- minix/servers/pm/memSelection.c
+++ minix/servers/pm/memSelection.c
@@ -9,6 +9,6 @@ int do_selectmempolicy(void){
     //CUSTOM_MEM_POLICY = 0;
     //printf("value of m_in.m1_i1 is %d\n", m_in.m1_i1);
     CUSTOM_MEM_POLICY = m_in.m1_i1;
-    printf("CUSTOM_MEM_POLICY is set to: %d\n", CUSTOM_MEM_POLICY);
+    printf("Memory Allocation Policy is set to: %d\n", CUSTOM_MEM_POLICY);
     return(OK);
 }
